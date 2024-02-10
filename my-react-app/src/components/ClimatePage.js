cb2b740ae6b96603080f91dc0eaff2acfedd30dd
martin0330 <martin.cruz0330@yahoo.com>
2024-02-10 17:39:00 -0600

    linked header components to its route

---
+++ my-react-app/src/components/ClimatePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ClimatePage() {
+    return (
+        <div>
+            <h2>Climate Page</h2>
+            <p1>Climate page content</p1>"
+        </div>
+    );
+}
+
+export default ClimatePage;
--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ClimatePage from './components/ClimatePage';
+import EnvironmentPage from './components/EnvironmentPage';
+import Workforce from './components/Workforce';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/ClimatePage" element={<ClimatePage />} />
+        <Route path="/EnvironmentPage" element={<EnvironmentPage />} />
+        <Route path="/Workforce" element={<Workforce />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
