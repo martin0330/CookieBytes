81c3e688c32995e46a079be3acd7be76ca416dce
martin0330 <martin.cruz0330@yahoo.com>
2024-02-10 23:10:24 -0600

    added definition fixed homepage

--- my-react-app/src/components/HomePage.js
+++ my-react-app/src/components/HomePage.js
@@ -1,15 +1,21 @@
 // HomePage.js
 import React from "react";
 import Header from "./Header";
+import './HomePage.css';
 
 function HomePage() {
     return (
-        <div>
-            <Header />
-            <h1>Home Page</h1>
-            <p style={{ position: 'absolute', bottom: 0, left: 0, padding: '10px' }}>
-                We are here to educate! Learn more about how COP takes into account sustainabitlity and keeps in touch with the environment.
-            </p>
+        <div className="container">
+            <video autoPlay muted loop id="myVideo" className="background-video">
+                <source src="webbackground.mp4" type="video/mp4" />
+            </video>
+            <div className="content">
+                <Header />
+                <div className="text-container">
+                    <h1 className="h1-style"> Sustainable{' '}<span className= "adjective-style">  adjective </span> </h1>
+                    <p className="definition-style">: of, relating to, or being a method of harvesting or using a resource so that the resource is not depleted or permanently damaged</p>
+                </div>
+            </div>
         </div>
     );
 }
