acdcb75d23cda7edefd49ec1f0cb084ff432ad4d
Yash Patel <yash.pateltx@gmail.com>
2024-02-10 19:53:20 -0600

    added mission

--- my-react-app/src/components/HomePage.js
+++ my-react-app/src/components/HomePage.js
@@ -1,3 +1,4 @@
+// HomePage.js
 import React from "react";
 import Header from "./Header";
 
@@ -5,7 +6,10 @@ function HomePage() {
     return (
         <div>
             <Header />
-            <h1> Home Page</h1>
+            <h1>Home Page</h1>
+            <p style={{ position: 'absolute', bottom: 0, left: 0, padding: '10px' }}>
+                We are here to educate! Learn more about how COP takes into account sustainabitlity and keeps in touch with the environment.
+            </p>
         </div>
     );
 }
